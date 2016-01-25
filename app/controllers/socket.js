4002ae7f6cf76e487026237ec248b66a198f6040
Cami Rodriguez <camiepisode@outlook.com>
2016-01-25 11:19:59 -0500

    Added a basic for this project

---
+++ app/controllers/socket.js
@@ -0,0 +1,41 @@
+function initialize(io, globals){
+	io.sockets.on('connection', function(socket){     
+
+		console.log('-' + socket.id);
+	
+		/// Welcome to the new client
+		socket.emit('Welcome', {SocketId : socket.id});
+				
+		/* 
+		================
+		Local functions 
+		*/
+		
+		/// Emit all cranes
+		function getCranes(){			
+			socket.emit('Backend.Message', {Command : 'CurrentPositionCranes', Values : globals.Cranes});
+		}
+		
+		/* 
+		================
+		Socket Intervals
+		*/
+		/// Emit every 5 seconds cranes
+		setInterval(getCranes, 5000);
+	
+		/* 
+		================
+		Templates for Socket.io
+		*/
+		/// Template for socket event
+		//socket.on('', function(data){
+		//  io.sockets.emit('Name', data);
+		// socket.emit('ID', {Command: 'CommandID', Values:[{ID: socket.id}]});
+		//});
+	
+	});
+		
+	console.log('Socket.io initialized');
+}
+
+exports.initialize = initialize;
